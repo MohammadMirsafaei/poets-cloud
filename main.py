adba0665fbcb748c9f564f2f276887d3ac6cea80
Mohammad Mirsafaei <info@mirsafaei.ir>
2020-03-03 21:34:24 +0330

    hafez verses extracted

--- main.py
+++ main.py
@@ -11,11 +11,28 @@ class DB:
             print("- Connected to database...\n")
         except Error as e:
             print(e)
+    
+    def select(self,query):
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
 
 
 def main():
     database = DB("ganjoor.s3db")
     database.connect();
+    rows = database.select("""select p.cat_id,v.text 
+                                from poem as p join verse as v on p.id=v.poem_id 
+                                where p.cat_id=24
+                            """)
+ 
+    f = open("verses.txt","w")
+    for r in rows:
+        f.write(r[1])
+        f.write('\n')
+    f.close()
+
 
 
 if __name__ == "__main__":
